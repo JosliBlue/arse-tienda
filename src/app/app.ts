a15d989fd88895efaef6b1bb2c02169fd1216147
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2026-07-24 21:23:00 -0500

    feat: header iniciado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, Header],
     templateUrl: './app.html',
-    styleUrl: './app.css',
 })
-export class App {
-    protected readonly title = signal('ArseTienda');
-}
+export class App {}

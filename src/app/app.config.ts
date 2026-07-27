1d9aa34e6c4342f1c82a30f234cbb464a075003c
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2026-07-27 09:19:51 -0500

    feat:sistema de env y creacion de env quitados

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from '@src/app/app.routes';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 export const appConfig: ApplicationConfig = {
-    providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(routes),
+        provideFirebaseApp(() =>
+            initializeApp({
+                apiKey: 'AIzaSyBV5PqEioYeGvVvFw77layA9VTzvSCxmh0',
+                authDomain: 'arse-tienda.firebaseapp.com',
+                projectId: 'arse-tienda',
+                storageBucket: 'arse-tienda.firebasestorage.app',
+                messagingSenderId: '459444078547',
+                appId: '1:459444078547:web:e14aca199459f009599126',
+            })
+        ),
+        provideFirestore(() => getFirestore()),
+    ],
 };
